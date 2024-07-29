e0d0b2cece2fc7c683be0b9892e13d018f68d51e
leerlandais <leerlandais@gmail.com>
2024-07-29 17:47:05 +0100

    view: added login form

--- view/public/public.home.view.php
+++ view/public/public.home.view.php
@@ -16,7 +16,7 @@
 <body class="bg-gradient-to-r from-gray-300 via-gray-400 to-gray-300">
 
 <div class="container mx-auto h-auto">
-    <div class="py-12">
+    <div class="pt-12 pb-6">
         <?php if(isset($errorMessage)) {
             ?>
             <h2 class="text-4xl text-red-600 text-center"><?=$errorMessage?></h2>
@@ -30,6 +30,9 @@
         <p class="leading-normal text-gray-800 text-base md:text-xl lg:text-2xl mb-8">
             Everything I need in one handy spot
         </p>
+        <?php
+        if(!isset($_GET["login"])) {
+        ?>
         <a href="?login">
             <button class="mx-auto lg:mx-0 hover:underline text-gray-800 font-extrabold rounded my-2 py-4 px-8 shadow-lg w-[25%] bg-white bg-opacity-50">
             Log In
@@ -38,12 +41,30 @@
         <a href="https://leerlandais.com" class="mx-auto lg:mx-0 hover:underline text-gray-800 font-extrabold rounded my-2 py-4 px-8 shadow-lg w-[25%] bg-white bg-opacity-50">
             Visit Home Site
         </a>
+        <?php
+        }
+        ?>
     </div>
 </div>
 <?php
 if (isset($_GET['login'])) {
     ?>
-    <p>Add login form</p>
+    <div class="flex items-center justify-center w-full dark:bg-gray-950 mb-4">
+        <div class="bg-gray-300 dark:bg-gray-900 shadow-md rounded-lg px-8 py-6 max-w-md text-center border border-black">
+            <form action="#">
+                <div class="mb-4">
+                    <label for="username" class="block text-sm font-medium text-black dark:text-gray-300 mb-2">User Name</label>
+                    <input type="text" name="username" class="bg-gray-200 shadow-sm text-center rounded-md w-full px-3 py-2 border border-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="user name" required>
+                </div>
+                <div class="mb-4">
+                    <label for="password" class="block text-sm font-medium text-black dark:text-gray-300 mb-2">Password</label>
+                    <input type="password" name="password" class="bg-gray-200 shadow-sm text-center rounded-md w-full px-3 py-2 border border-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="password" required>
+
+                </div>
+                <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Login</button>
+            </form>
+        </div>
+    </div>
 <?php
 }
 ?>
