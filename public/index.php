a64f0c7c017af9e194f36525d89ec3e38dd7247a
leerlandais <leerlandais@gmail.com>
2024-08-07 16:37:32 +0100

    security: added timeout on inactivity

--- public/index.php
+++ public/index.php
@@ -1,36 +1,41 @@
 <?php
-session_start(); // always start with the session :)
-// my new way of handling error messages - this permits message to exist even after page reload
+session_start();
+// CHECK SESSION ACTIVITY OR LOGOUT AUTOMATICALLY
+if (isset($_SESSION["active"]) && time() - $_SESSION["active"] > 180) {
+    require_once "../model/logoutModel.php";
+    exit();
+}
+$_SESSION["active"] = time();
+
 if (isset($_SESSION["errorMessage"])) {
     $errorMessage = $_SESSION["errorMessage"];
     unset($_SESSION["errorMessage"]);
 }
 
-use model\MyPDO; // then set up a DB connection
+use model\MyPDO;
 
-require_once "../config.php"; // always need the config and ALWAYS MAKE SURE THERE'S A .GITIGNORE
+require_once "../config.php";
 
-// autoload the Classes
+
 spl_autoload_register(function ($class) {
     $class = str_replace('\\', '/', $class);
     require PROJECT_DIRECTORY . '/' . $class . '.php';
 });
 
 try {
-// connect to the DB
+    echo 'hello'; // THIS TEST WORKS FINE
     $db = MyPDO::getInstance(DB_TYPE . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT . ";charset=" . DB_CHARSET,
         DB_LOGIN,
         DB_PWD);
-// set attributes for error handling and fetch modes
+
     $db->setAttribute(MyPDO::ATTR_ERRMODE, MyPDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    var_dump($db); // THIS DOES NOT SHOW ONLINE BUT WORKS FINE WITH WAMP
 } catch (Exception $e) {
     die($e->getMessage());
 }
 
-// Call the first router - handles basic necessities and directs to other controllers
 require_once PROJECT_DIRECTORY . '/controller/routerController.php';
-
 
 $db = null;
 
